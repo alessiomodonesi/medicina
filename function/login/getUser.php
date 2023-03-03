58c6f03c13743084173ad81fd56ab7da4eeadb98
alessiomodonesi <alessiomodo2004@gmail.com>
2023-03-03 23:40:51 +0100

    getuser funzionante

---
+++ function/login/getUser.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Content-type: application/json; charset=UTF-8");
+include_once dirname(__FILE__) . '/../connect.php';
+include_once dirname(__FILE__) . '/../model.php';
+
+if (!isset($_GET['id'])) {
+    http_response_code(400);
+    echo json_encode(["message" => "Insert the id param"]);
+    exit();
+}
+
+$id = explode("?id=", $_SERVER["REQUEST_URI"])[1];
+
+if (empty($id)) {
+    http_response_code(404);
+    echo json_encode(["message" => "Insert a valid ID"]);
+    exit();
+}
+
+$database = new Database();
+$db = $database->connect();
+
+$user = new Model($db);
+$user->getUser($id);
--- function/model.php
+++ function/model.php
@@ -9,6 +9,41 @@ class Model
         $this->conn = $db;
     }
 
+    function getUser($id)
+    {
+        $sql = sprintf(
+            "SELECT id, name, surname, email
+            FROM user
+            WHERE id = %d;",
+            $this->conn->real_escape_string($id)
+        );
+
+        $result = $this->conn->query($sql);
+        $this->SendOutput($result, JSON_PRETTY_PRINT);
+    }
+
+    function SendOutput($data, $headers = array())
+    {
+        $this->SetHeaders($headers);
+
+        $arr = array();
+        while ($row = $data->fetch_assoc()) {
+            array_push($arr, $row);
+        }
+        print_r(json_encode($arr, JSON_PRETTY_PRINT));
+        exit;
+    }
+
+    function SetHeaders($httpHeaders = array())
+    {
+        header_remove('Set-Cookie');
+        if (is_array($httpHeaders) && count($httpHeaders)) {
+            foreach ($httpHeaders as $httpHeader) {
+                header($httpHeader);
+            }
+        }
+    }
+
     function getArchiveActivity()
     {
         $query = "SELECT DISTINCT p.codice, p.nome FROM piano_di_studi p
