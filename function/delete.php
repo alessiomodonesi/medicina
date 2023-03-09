726d6c8b633f0364735d82a81ffe16285154d99c
alessiomodonesi <alessiomodo2004@gmail.com>
2023-03-09 12:17:28 +0100

    Update delete.php

--- function/delete.php
+++ function/delete.php
@@ -8,11 +8,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $conn = $db->connect();
 
         $query = sprintf(
-            "DELETE FROM piano_di_studi WHERE codice = '%s'",
+            "SELECT fd.didattica 
+            FROM formativa_didattica fd 
+            INNER JOIN piano_di_studi pd ON pd.codice = fd.formativa
+            WHERE pd.codice = '%s'",
             $_POST['codice']
         );
 
-        $conn->query($query);
+        $result =  $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            unset($query);
+            $query = sprintf("DELETE FROM formativa_didattica WHERE formativa = '%s'", $id);
+            $conn->query($query);
+
+            while ($row = $result->fetch_assoc()) {
+                unset($query);
+                $query = sprintf("DELETE FROM piano_di_studi WHERE codice = '%s'", $row['didattica']);
+                $conn->query($query);
+            }
+        }
+
+        unset($query);
+        $query = sprintf("DELETE FROM piano_di_studi WHERE codice = '%s'", $id);
+        $conn->query($sql);
+
         header("Location: http://localhost/registro?page=1");
     }
 }
